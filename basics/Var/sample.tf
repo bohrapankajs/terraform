c4aac1698fc45f5c0352b3ec976e9e4038b7bde7
Pankajbohra <bohrapankajs@gmail.com>
2023-06-17 17:28:16 +0530

    test

--- basics/Var/sample.tf
+++ basics/Var/sample.tf
@@ -10,3 +10,17 @@ output "sample1" {
 
 value = "He said: my name is ${var.var1}"
 }
+
+variable "list-res" {
+default = [
+    "Pankaj",
+    "Sanket",
+    0001,
+    "sama00",
+    false
+]
+}
+
+output "sample2" {
+    value = "Name of persons are ${var.list-res[0]} and ${var.list-res[1]} with address number ${var.list-res[3]} and last ${var.list-res[4]} "
+}
