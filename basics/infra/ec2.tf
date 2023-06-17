30b0d8ff42ac894ee7bea20249f154d829816070
Pankajbohra <bohrapankajs@gmail.com>
2023-06-17 18:21:21 +0530

    test

--- basics/infra/ec2.tf
+++ basics/infra/ec2.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "allow_ssh" {
   description = "Allow SSH inbound traffic"
 
     ingress {
-      description      = Allow SSH From Public
+      description      = "Allow SSH From Public"
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
@@ -27,7 +27,7 @@ resource "aws_security_group" "allow_ssh" {
     }
   
     ingress {
-      description      = Allow HTTP From Public
+      description      = "Allow HTTP From Public"
       from_port        = 80
       to_port          = 80
       protocol         = "tcp"
