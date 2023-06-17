0b8f8ed9302f388624ad3de9434cbe98bde909e8
Pankajbohra <bohrapankajs@gmail.com>
2023-06-17 17:29:17 +0530

    test

--- basics/Var/sample.tf
+++ basics/Var/sample.tf
@@ -3,9 +3,9 @@ variable "var1" {
 
 }
 
-output "sample" {
-    value = var.var1
-}
+# output "sample" {
+#     value = var.var1
+# }
 output "sample1" {
 
 value = "He said: my name is ${var.var1}"
