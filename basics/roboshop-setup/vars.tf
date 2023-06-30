25e5cf7080e0812c8f9f97628ff5d171aff67d58
Pankajbohra <bohrapankajs@gmail.com>
2023-06-30 15:07:53 +0530

    test

--- basics/roboshop-setup/vars.tf
+++ basics/roboshop-setup/vars.tf
@@ -6,32 +6,32 @@ variable "ALL_COMPONENTS" {
 mongodb   =  {
             app_version  =  "null"
         },
-        catalogue   =  {
-            app_version  =  "0.0.2"
-        },
-        redis   =  {
-            app_version  =  "null"
-        },
-        cart   =  {
-            app_version  =  "0.0.2"
-        },
-        user   =  {
-            app_version  =  "0.0.2"
-        },
-        mysql   =  {
-            app_version  =  "null"
-        },
-        shipping   =  {
-            app_version  =  "0.0.1"
-        },
-        rabbitmq   =  {
-            app_version  =  "null"
-        },
-        payment   =  {
-            app_version  =  "0.0.2"
-        },
-        frontend   =  {
-            app_version  =  "0.0.3"
-        }
+        # catalogue   =  {
+        #     app_version  =  "0.0.2"
+        # },
+        # redis   =  {
+        #     app_version  =  "null"
+        # },
+        # cart   =  {
+        #     app_version  =  "0.0.2"
+        # },
+        # user   =  {
+        #     app_version  =  "0.0.2"
+        # },
+        # mysql   =  {
+        #     app_version  =  "null"
+        # },
+        # shipping   =  {
+        #     app_version  =  "0.0.1"
+        # },
+        # rabbitmq   =  {
+        #     app_version  =  "null"
+        # },
+        # payment   =  {
+        #     app_version  =  "0.0.2"
+        # },
+        # frontend   =  {
+        #     app_version  =  "0.0.3"
+        # }
     }
 }
